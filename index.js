2ffa6461ff9806519d797fd8b84a077f6d3530ed
Highscoretech <highscoretechng@gmail.com>
2024-11-26 13:07:53 -0600

    god abek 3

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ app.use(function (req, res, next) {
 });
 
 mongoose.set('strictQuery', false);
-//   const dbUri = `mongodb+srv://highscoreteh:AoUXugCyZEfpBmMx@cluster0.xmpkpjc.mongodb.net/mevbot?retryWrites=true&w=majority`
-const dbUri = `mongodb://localhost:27017/mevbot`;
+  const dbUri = `mongodb+srv://highscoreteh:AoUXugCyZEfpBmMx@cluster0.xmpkpjc.mongodb.net/mevbot?retryWrites=true&w=majority`
+// const dbUri = `mongodb://localhost:27017/mevbot`;
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => console.log('Database connected'))
     .catch((err) => console.log(err))
